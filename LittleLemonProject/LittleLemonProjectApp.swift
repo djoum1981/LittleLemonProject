e1a9cbb7c0a59f4bf361c821b1e74419f23c8474
Julien Laurent <julienlaurent@my.uopeople.edu>
2023-05-01 20:01:47 -0400

    Initial Commit

---
+++ LittleLemonProject/LittleLemonProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonProjectApp.swift
+//  LittleLemonProject
+//
+//  Created by Julien Laurent on 5/1/23.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
