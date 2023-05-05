1db1b5f8b08f254302861d3f9af019a5edfb1f33
Julien <julienlaurent@my.uopeople.edu>
2023-05-05 17:16:53 -0400

    dabase added

--- LittleLemonProject/Database/DataManager.swift
+++ LittleLemonProject/Database/DataManager.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import CoreData
+struct Datamanager{
+    static var shared = Datamanager()
+    let  persitentContainer: NSPersistentContainer
+    
+    private init() {
+        self.persitentContainer = NSPersistentContainer(name: "LittleLemonProjectDB")
+        self.persitentContainer.loadPersistentStores { desc, error in
+            if let error = error{
+                fatalError("Unable to load the database \(error.localizedDescription)")
+            }
+        }
+        persitentContainer.viewContext.automaticallyMergesChangesFromParent = true
+    }
+    
+    func context()-> NSManagedObjectContext {
+        return persitentContainer.viewContext
+    }
+}
